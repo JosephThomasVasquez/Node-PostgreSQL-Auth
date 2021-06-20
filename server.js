c4c26dfab04432ed36706c589e68476931c58494
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-06-20 16:34:59 -0700

    Added dotenv package and setup server to listen on port with .env

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-import express from 'express';
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+
+// Run server and listen on PORT
+app.listen(PORT, () => {
+  console.log("Server running on port: ", PORT);
+});
