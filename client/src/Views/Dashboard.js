2f31c7bdc4cc3ac8fc17e9a913932348ccd35b43
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-06-21 16:18:19 -0700

    Added Dashboard elements

--- client/src/Views/Dashboard.js
+++ client/src/Views/Dashboard.js
@@ -1,14 +1,72 @@
 import React from "react";
 import NavBar from "../components/NavBar";
 import Container from "@material-ui/core/Container";
+import {
+  FormControl,
+  FormLabel,
+  FormHelperText,
+  Input,
+  InputLabel,
+  Button,
+} from "@material-ui/core";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import { lightBlue } from "@material-ui/core/colors";
+import Typography from "@material-ui/core/Typography";
+import { Link } from "@material-ui/core";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      type: "light",
+      main: lightBlue[400],
+      contrastText: "white",
+    },
+    secondary: {
+      // This is green.A700 as hex.
+      main: "#11cb5f",
+      contrastText: "white",
+    },
+  },
+});
 
 const Dashboard = () => {
   return (
     <>
       <NavBar />
-      <Container>
-        <h1>Dashboard</h1>
-      </Container>
+      <ThemeProvider theme={theme}>
+        <Container variant="dark" maxWidth="sm">
+          <h1>Dashboard</h1>
+
+          <form action="/users/register" method="POST" className="form-bg">
+            <FormControl className="form-control">
+              <InputLabel className="input-labels">Name</InputLabel>
+              <Input id="input-name" aria-describedby="Enter your name" />
+            </FormControl>
+
+            <FormControl className="form-control">
+              <InputLabel className="input-labels">Email</InputLabel>
+              <Input
+                id="input-email"
+                aria-describedby="Enter your email address"
+              />
+            </FormControl>
+
+            <FormControl className="form-control">
+              <InputLabel className="input-labels">Password</InputLabel>
+              <Input
+                id="input-password"
+                aria-describedby="Please enter a strong password"
+              />
+            </FormControl>
+
+            <div className="form-control button-register">
+              <Button variant="contained" color="primary" type="submmit">
+                Logout
+              </Button>
+            </div>
+          </form>
+        </Container>
+      </ThemeProvider>
     </>
   );
 };
