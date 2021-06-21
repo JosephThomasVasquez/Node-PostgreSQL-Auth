7c657c43c2683ed23a671c2615779bf363450ba7
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-06-20 19:57:03 -0700

    Added styling to the form controls

--- client/src/Views/Register.js
+++ client/src/Views/Register.js
@@ -6,6 +6,7 @@ import {
   FormHelperText,
   Input,
   InputLabel,
+  Button,
 } from "@material-ui/core";
 import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 import { lightBlue } from "@material-ui/core/colors";
@@ -13,12 +14,14 @@ import { lightBlue } from "@material-ui/core/colors";
 const theme = createMuiTheme({
   palette: {
     primary: {
-      type: "dark",
+      type: "light",
       main: lightBlue[400],
+      contrastText: 'white'
     },
     secondary: {
       // This is green.A700 as hex.
       main: "#11cb5f",
+      contrastText: 'white'
     },
   },
 });
@@ -30,7 +33,7 @@ const Register = () => {
         <h1>Register</h1>
 
         <form action="/users/register" method="POST" className="form-bg">
-          <FormControl>
+          <FormControl className="form-control">
             <InputLabel className="input-labels">Name</InputLabel>
             <Input id="input-name" aria-describedby="Enter your name" />
             <FormHelperText id="Enter your name">
@@ -38,21 +41,42 @@ const Register = () => {
             </FormHelperText>
           </FormControl>
 
-          <FormControl>
+          <FormControl className="form-control">
             <InputLabel className="input-labels">Email</InputLabel>
-            <Input id="input-email" aria-describedby="Enter your email address" />
+            <Input
+              id="input-email"
+              aria-describedby="Enter your email address"
+            />
             <FormHelperText id="Enter your email address">
               Where we can reach you!
             </FormHelperText>
           </FormControl>
 
-          <FormControl>
+          <FormControl className="form-control">
             <InputLabel className="input-labels">Password</InputLabel>
-            <Input id="input-password" aria-describedby="Please enter a strong password" />
+            <Input
+              id="input-password"
+              aria-describedby="Please enter a strong password"
+            />
             <FormHelperText id="Please enter a strong password">
-            A strong password contains special characters, uppercase, lowercase, and numbers.
+              A strong password contains special characters, uppercase,
+              lowercase, and numbers.
             </FormHelperText>
           </FormControl>
+
+          <FormControl className="form-control">
+            <InputLabel className="input-labels">Confirm Password</InputLabel>
+            <Input
+              id="input-password"
+              aria-describedby="Please confirm password"
+            />
+            <FormHelperText id="Please confirm password">
+              Enter the password again to confirm it.
+            </FormHelperText>
+          </FormControl>
+          <Button variant="contained" color="primary">
+            Register
+          </Button>
         </form>
       </Container>
     </ThemeProvider>
