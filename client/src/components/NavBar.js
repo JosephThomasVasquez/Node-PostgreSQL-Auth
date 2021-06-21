111ff795ff37fd1cbfbaef87c3ce6b8581355072
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-06-20 20:11:22 -0700

    Added styling and icons package to navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,12 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import Button from "@material-ui/core/Button";
+import MenuIcon from "@material-ui/icons/Menu";
+// import ModalDialog from "./ModalDialog";
+
+const useStyles = makeStyles((theme) => ({
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 const NavBar = () => {
+  const classes = useStyles();
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <nav>
-      <ul>
-        <li></li>
-      </ul>
-    </nav>
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          className={classes.menuButton}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" className={classes.title}>
+          User Authentication
+        </Typography>
+        <Button color="inherit" onClick={handleOpen}>
+          Login
+        </Button>
+        <Button color="inherit" onClick={handleOpen}>
+          Register
+        </Button>
+      </Toolbar>
+      {/* <ModalDialog open={open} handleClose={handleClose} /> */}
+    </AppBar>
   );
 };
 
