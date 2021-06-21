bd73e650848404e6acc3cf49df175cda66ac9ca1
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-06-21 16:26:53 -0700

    Added elements and links to register and login pages

--- client/src/Views/Home.js
+++ client/src/Views/Home.js
@@ -1,7 +1,33 @@
 import React, { useState, useEffect } from "react";
 import NavBar from "../components/NavBar";
 import Container from "@material-ui/core/Container";
-import Button from "@material-ui/core/Button";
+import {
+  FormControl,
+  FormLabel,
+  FormHelperText,
+  Input,
+  InputLabel,
+  Button,
+} from "@material-ui/core";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import { lightBlue } from "@material-ui/core/colors";
+import Typography from "@material-ui/core/Typography";
+import { Link } from "@material-ui/core";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      type: "light",
+      main: lightBlue[400],
+      contrastText: "white",
+    },
+    secondary: {
+      // This is green.A700 as hex.
+      main: "#11cb5f",
+      contrastText: "white",
+    },
+  },
+});
 
 const Home = () => {
   const [data, setData] = useState("");
@@ -28,13 +54,36 @@ const Home = () => {
   return (
     <>
       <NavBar />
-      <Container>
-        <h1>Home {data && data.data}</h1>
-        <Button variant="contained" color="primary">
-          Login
-        </Button>
-        {JSON.stringify(data)}
-      </Container>
+      <ThemeProvider theme={theme}>
+        <Container maxWidth="sm">
+          <h1>Home {data && data.data}</h1>
+          <div className="text-center">
+            <Button variant="contained" color="primary" type="submmit">
+              <Typography variant="body1" className="">
+                <Link href="/users/login" color="inherit" className="nav-link">
+                  Login
+                </Link>
+              </Typography>
+            </Button>
+          </div>
+          <div className="text-center">
+            New users
+            <Button color="primary" type="submmit">
+              <Typography variant="body1" className="">
+                <Link
+                  href="/users/register"
+                  color="inherit"
+                  className="nav-link"
+                >
+                  Register here
+                </Link>
+              </Typography>
+            </Button>
+          </div>
+
+          {JSON.stringify(data)}
+        </Container>
+      </ThemeProvider>
     </>
   );
 };
