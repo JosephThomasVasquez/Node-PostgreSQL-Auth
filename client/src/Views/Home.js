684cba2f43944b8d633175987383e4dd73dce1a5
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-06-20 17:47:22 -0700

    changed background to dark grey and text to white

--- client/src/Views/Home.js
+++ client/src/Views/Home.js
@@ -1,11 +1,15 @@
 import React from "react";
 import NavBar from "../components/NavBar";
+import Button from "@material-ui/core/Button";
 
 const Home = () => {
   return (
     <>
       <NavBar />
       <h1>Home</h1>
+      <Button variant="contained" color="primary">
+        Login
+      </Button>
     </>
   );
 };
