8d201391b500a8ad3e55b867adad5a3c867769ef
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-06-21 19:16:10 -0700

    Added some input validation error handling

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -11,7 +11,35 @@ const registerUser = (req, res) => {
     password,
     confirmPassword,
   });
-  
+
+  const errors = [];
+  const errorz = [
+    { message: "Please fill out all input fields!" },
+    { message: "Password must be at least 8 characters long." },
+    { message: "Passwords do not match!" },
+  ];
+
+  // INPUT VALIDATION ----------------------------------------------------------
+
+  // ERROR: Fill out all fields
+  if (!name || !email || !password || confirmPassword) {
+    errors.push({ message: "Please fill out all input fields!" });
+  }
+
+  // Password minimum length 8 characters
+  if (password.length < 8) {
+    errors.push({ message: "Password must be at least 8 characters long." });
+  }
+
+  // Passwords do not match
+  if (password !== confirmPassword) {
+    errors.push({ message: "Passwords do not match!" });
+  }
+
+  if (errors.length > 0) {
+    console.log(errors);
+    res.json(errors);
+  }
 };
 
 export { registerUser };
