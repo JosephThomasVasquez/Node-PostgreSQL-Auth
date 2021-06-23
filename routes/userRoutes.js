374f767ecac1c1aafc3395768d2d930a28e65841
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-06-22 17:10:31 -0700

    Now successfully connects to postgresql database

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -47,7 +47,7 @@ const registerUser = async (req, res) => {
       [email],
       (error, results) => {
         if (error) {
-          console.log(error);
+          console.log(results);
           throw error;
         }
         console.log(results.rows);
