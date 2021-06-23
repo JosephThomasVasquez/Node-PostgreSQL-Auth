b999950a190820c8d1814c4dd3cbc67f26f698d1
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-06-22 18:48:44 -0700

    Added styling to success message component

--- client/src/components/SuccessMessage.js
+++ client/src/components/SuccessMessage.js
@@ -3,7 +3,7 @@ import React from "react";
 const SuccessMessage = ({ message }) => {
   return (
     <div>
-      <p className="error-msg">{message}</p>
+      <p className="success-msg">{message}</p>
     </div>
   );
 };
